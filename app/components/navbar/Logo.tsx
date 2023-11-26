a36507a8ea3c6a4eb8e797c1e0b3d05d8f67a68b
kenan551 <kenan.ramazanov1104@gmail.com>
2023-11-26 11:36:12 +0400

    Creating Logo

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,12 @@
+"use client"
+import { useRouter } from 'next/navigation'
+import React from 'react'
+
+const Logo = () => {
+    const router =useRouter();
+  return (
+    <div>Logo</div>
+  )
+}
+
+export default Logo
--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React from 'react'
 import Container from '../Container'
+import Logo from './Logo'
 
 const Navbar = () => {
   return (
@@ -10,8 +11,9 @@ const Navbar = () => {
   >
 <Container>
   <div
-  className='flex flex-row items-center justify-between gap-3'>
-
+  className='flex flex-row items-center
+   justify-between gap-3 md:gap-0'>
+  <Logo/>
   </div>
 </Container>
   </div>
