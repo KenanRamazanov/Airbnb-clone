e681e5cd3914a280a38e91a9a73230559df5efdd
kenan551 <kenan.ramazanov1104@gmail.com>
2023-11-26 12:39:52 +0400

    Final of Navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Container from '../Container'
 import Logo from './Logo'
 import Search from './Search'
+import UserMenu from './UserMenu'
 
 const Navbar = () => {
   return (
@@ -16,6 +17,7 @@ const Navbar = () => {
    justify-between gap-3 md:gap-0'>
   <Logo/>
   <Search/>
+  <UserMenu/>
   </div>
 </Container>
   </div>
