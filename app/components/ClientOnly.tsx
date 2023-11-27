56266ffc31fee5219facace7e5382173aa1c0015
kenan551 <kenan.ramazanov1104@gmail.com>
2023-11-27 22:24:23 +0400

    Creating Modal

--- app/components/ClientOnly.tsx
+++ app/components/ClientOnly.tsx
@@ -1,10 +1,20 @@
 "use client"
-import React, { useState } from 'react'
-
-const ClientOnly = () => {
+import React, { useEffect, useState } from 'react'
+interface ClientOnlyProps{
+    children: React.ReactNode
+}
+const ClientOnly: React.FC<ClientOnlyProps> = ({children}) => {
     const [hasMounted,setHasMounted] = useState(false)
+    useEffect(()=>{
+    setHasMounted(true)
+    },[])
+    if(!hasMounted){
+        return null
+    }
   return (
-    <div>ClientOnly</div>
+  <>
+  {children}
+  </>
   )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import {Nunito} from 'next/font/google';
 import './globals.css'
 import Navbar from './components/navbar/Navbar';
+import ClientOnly from './components/ClientOnly';
+import Modal from './components/modals/Modal';
 
 
 export const metadata = {
@@ -21,7 +23,10 @@ export default function RootLayout({
     <html lang="en">
       <body
        className={font.className}>
+        <ClientOnly>
+          <Modal/>
         <Navbar/>
+        </ClientOnly>
         {children}
         </body>
     </html>
