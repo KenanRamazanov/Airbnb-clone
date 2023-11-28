476a600b26057eb4fef3786fd7247d222cf16d32
kenan551 <kenan.ramazanov1104@gmail.com>
2023-11-29 00:23:20 +0400

    Final of Button

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -135,7 +135,7 @@ const Modal: React.FC<ModalProps> = ({
     <div className='flex flex-col gap-2 p-6'>
     <div className='flex flex-row 
     items-center gap-4 w-full'>
-   <Button/>
+   <Button label='My Button'/>
     </div>
     </div>
     </div> 
