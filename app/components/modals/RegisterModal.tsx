7e51caefac8aae4630e05bcc6834493b66727a06
kenan551 <kenan.ramazanov1104@gmail.com>
2023-11-29 20:45:27 +0400

    Creating RegisterPage

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -1,5 +1,15 @@
+"use client"
 import React from 'react'
-
+import axios from "axios";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { useCallback, useState } from "react";
+import { 
+  FieldValues, 
+  SubmitHandler,
+  useForm
+} from "react-hook-form";
+import useRegisterModal from '@/app/hooks/useRegisterModal';
 const RegisterModal = () => {
   return (
     <div>
