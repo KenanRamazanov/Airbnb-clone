f862f29c8c482c7ef950803a7a8ebda493e113b8
kenan551 <kenan.ramazanov1104@gmail.com>
2023-11-29 22:38:06 +0400

    Final of Header

--- app/components/Heading.tsx
+++ app/components/Heading.tsx
@@ -1,8 +1,25 @@
+"use client"
 import React from 'react'
-
-const Heading = () => {
+interface HeadingProps{
+    title: string;
+    subtitle?: string;
+    center?: boolean;
+}
+const Heading: React.FC<HeadingProps> = ({
+    title,
+    subtitle,
+    center
+}) => {
   return (
-    <div>Heading</div>
+    <div className={center ? 'text-center' :'text-start'}>
+   <div className='text-2xl font-bold'>
+   {title}
+   </div>
+   <div className='font-light text-neutral-500 mt-2'>
+   {subtitle}
+   </div>
+    </div>
+ 
   )
 }
 
