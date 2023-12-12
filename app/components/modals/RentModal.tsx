3a4e658eeb477d19606f95cbe1320afdfe939fb6
kenan551 <kenan.ramazanov1104@gmail.com>
2023-12-12 20:33:41 +0400

    Creating Image Section

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -56,6 +56,7 @@ const {
   const guestCount = watch('guestCount');
   const roomCount = watch('roomCount');
   const bathroomCount = watch('bathroomCount');
+  const imageSrc = watch('imageSrc');
   const Map = useMemo(() => dynamic(() => import('../Map'), { 
     ssr: false 
   }), [location]);
