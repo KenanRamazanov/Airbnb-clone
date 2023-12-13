363b5d85957a72d409b489e60f21fcc4b0e315fb
kenan551 <kenan.ramazanov1104@gmail.com>
2023-12-13 19:14:47 +0400

    Final of Price

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -217,8 +217,22 @@ let bodyContent = (
    }
 if(step === STEPS.PRICE) {
     bodyContent = (
-        <div className=''>
+        <div className='flex flex-col gap-8'>
+       <Heading
+        title="Now, set your price"
+        subtitle="How much do you charge per night?"
+       />
 
+       <Input
+       id="price"
+       label="Price"
+       formatPrice 
+       type="number" 
+       disabled={isLoading}
+       register={register}
+       errors={errors}
+       required
+       />
         </div>
     )
 }
