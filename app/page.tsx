65e3b12136eac40dbbf951721d3ada0330409fbc
kenan551 <kenan.ramazanov1104@gmail.com>
2023-12-13 23:20:34 +0400

    Creating EmptyState

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,37 @@
-import Image from 'next/image'
+
+import ClientOnly from './components/ClientOnly'
+import Container from './components/Container'
+import EmptyState from './components/EmptyState';
 
 export default function Home() {
+  const isEmpty = true;
+  if(isEmpty) {
+    return (
+      <ClientOnly>
+        <EmptyState/>
+      </ClientOnly>
+    )
+  }
   return (
-  <div className='text-rose-500 text-2xl'>Hello Airbnb</div>
+<ClientOnly>
+  <Container >
+ <div  className="
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8"
+            >
+     <div>
+      My future Listings
+     </div>
+
+ </div>
+  </Container>
+</ClientOnly>
   )
 }
