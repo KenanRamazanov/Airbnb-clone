293aa5521f0229b581bbc27b16c15f7f7135bfe3
kenan551 <kenan.ramazanov1104@gmail.com>
2023-12-17 23:27:17 +0400

    Creating ListingClient

---
+++ app/listings/[listingId]/ListingClient.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ListingClient = () => {
+  return (
+    <div>ListingClient</div>
+  )
+}
+
+export default ListingClient
--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -1,11 +1,37 @@
+import getCurrentUser from '@/app/actions/getCurrentUser';
+import getListingById from '@/app/actions/getListingById';
+import ClientOnly from '@/app/components/ClientOnly';
+import EmptyState from '@/app/components/EmptyState';
 import React from 'react'
+import ListingClient from './ListingClient';
 
-const page = () => {
+
+interface IParams {
+    listingId?: string;
+  }
+  
+
+const ListingPage = async ({params} : { params: IParams } ) => {
+
+const listing = await getListingById(params);
+const currentUser = await getCurrentUser();
+if(!listing) {
+    return (
+        <ClientOnly>
+        <EmptyState/>
+        </ClientOnly>
+    );
+}
   return (
     <div>
-        My individual listing page!
+     <ClientOnly>
+        <ListingClient
+        // listing={listing}
+        // currentUser={currentUser}
+        />
+     </ClientOnly>
     </div>
   )
 }
 
-export default page
+export default ListingPage;
