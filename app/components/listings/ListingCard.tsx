bcf570fa57602d481bd554298ab07842a4e5c0df
kenan551 <kenan.ramazanov1104@gmail.com>
2023-12-17 11:50:37 +0400

    Final of ListingCard

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -7,6 +7,7 @@ import { useRouter } from 'next/navigation';
 import useCountries from '@/app/hooks/useCountries';
 import { format } from 'date-fns';
 import HeartButton from '../HeartButton';
+import Button from '../Button';
 
 interface ListingCardProps {
   data: Listing;
@@ -82,6 +83,28 @@ const location = getByValue(data.locationValue);
         />
           </div>
     </div>
+    <div className='font-semibold text-lg'>
+   {location?.region}, {location?.label}
+    </div>
+   <div className='font-light text-neutral-500'>
+   {reservationDate || data.category }
+   </div>
+   <div className='flex flex-row items-center gap-1'>
+     <div className='font-semibold '>
+      ${price}
+     </div>
+     {! reservation && (
+      <div className='font-light'>night</div>
+     )}
+   </div>
+   {onAction && actionLabel && (
+    <Button
+    disabled={disabled}
+    small
+    label={actionLabel}
+    onClick={handleCancel}
+    />
+   )}
     </div>
     </div>
   )
