3265cee35445b51daf23de147dbe8e20769ac370
kenan551 <kenan.ramazanov1104@gmail.com>
2023-12-22 01:14:35 +0400

    Final commit

--- app/components/Loader.tsx
+++ app/components/Loader.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+'use client';
+
+import { PuffLoader } from "react-spinners";
 
 const Loader = () => {
-  return (
-    <div>Loader</div>
-  )
+  return ( 
+    <div
+    className="
+      h-[70vh]
+      flex 
+      flex-col 
+      justify-center 
+      items-center 
+    "
+    >
+      <PuffLoader
+        size={100}
+        color="red"
+      />
+    </div>
+   );
 }
-
-export default Loader
+ 
+export default Loader;
--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,9 +1,46 @@
 "use client"
+import useCountries from '@/app/hooks/useCountries';
 import useSearchModal from '@/app/hooks/useSearchModal';
-import React from 'react'
+import { differenceInDays } from 'date-fns';
+import { useSearchParams } from 'next/navigation';
+import React, { useMemo } from 'react'
 import { BiSearch } from 'react-icons/bi';
 const Search = () => {
   const searchModal = useSearchModal();
+  const params = useSearchParams();
+  const { getByValue } = useCountries();
+  const  locationValue = params?.get('locationValue'); 
+  const  startDate = params?.get('startDate');
+  const  endDate = params?.get('endDate');
+  const  guestCount = params?.get('guestCount');
+  const locationLabel = useMemo(() => {
+    if (locationValue) {
+      return getByValue(locationValue as string)?.label;
+    }
+    return 'Anywhere';
+  }, [locationValue, getByValue]);
+  const durationLabel = useMemo(() => {
+    if (startDate && endDate) {
+      const start = new Date(startDate as string);
+      const end = new Date(endDate as string);
+      let diff = differenceInDays(end, start);
+
+      if (diff === 0) {
+        diff = 1;
+      }
+
+      return `${diff} Days`;
+    }
+
+    return 'Any Week'
+  }, [startDate, endDate]);
+  const guestLabel = useMemo(() => {
+    if (guestCount) {
+      return `${guestCount} Guests`;
+    }
+
+    return 'Add Guests';
+  }, [guestCount]);
   return (
     <div 
     onClick={searchModal.onOpen}
@@ -24,7 +61,7 @@ const Search = () => {
     <div className='text-sm 
     font-semibold
      px-6'>
-     Anywhere
+    {locationLabel}
     </div>
     <div className='hidden 
     sm:block
@@ -34,7 +71,7 @@ const Search = () => {
        border-x-[1px] 
        flex-1
         text-center'>
-     Any Week
+    {durationLabel}
     </div>
     <div className='text-sm 
     pl-6
@@ -44,7 +81,7 @@ const Search = () => {
     flex-row 
     items-center
      gap-3'>
-   <div className='hidden sm:block'> Add Guests</div>
+   <div className='hidden sm:block'> {guestLabel}</div>
    <div className='p-2
     bg-rose-500 
     rounded-full
